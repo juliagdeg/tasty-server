002bcdd3d516a629e119e6563a496e1def33a6e7
Julia Degnan <juliagdegnan@gmail.com>
2023-06-14 13:50:10 -0500

    added category view + url

--- tasty/urls.py
+++ tasty/urls.py
@@ -2,10 +2,11 @@ from django.contrib import admin
 from django.conf.urls import include
 from django.urls import path
 from rest_framework import routers
-from tastyapi.views import register_user, login_user, RecipeView
+from tastyapi.views import register_user, login_user, RecipeView, CategoryView
 
 router = routers.DefaultRouter(trailing_slash=False)
 router.register(r"recipes", RecipeView, "recipe")
+router.register(r"categories", CategoryView, "category")
 
 urlpatterns = [
     path('register', register_user),
--- tastyapi/views/__init__.py
+++ tastyapi/views/__init__.py
@@ -1,2 +1,3 @@
 from .auth import login_user, register_user
 from .recipe_view import RecipeView
+from .category_view import CategoryView
