b13817b27e2dce384699ffd0da1fce75453ce485
Julia Degnan <juliagdegnan@gmail.com>
2023-06-20 10:34:01 -0500

    comment feature added

--- tastyapi/models/comment.py
+++ tastyapi/models/comment.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Comment(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    recipe = models.ForeignKey("Recipe", on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comments')
+    recipe = models.ForeignKey("Recipe", on_delete=models.CASCADE, related_name='comments')
     content = models.CharField(max_length=300)
     postdate = models.DateTimeField()
